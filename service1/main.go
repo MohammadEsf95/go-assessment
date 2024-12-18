9333c211d065a53aed01b1977ad9c5fc609110ea
MohammadEsf95 <mh.esfahani95@gmail.com>
2024-12-18 17:35:17 +0330

    wip: implement grpc server in service1

--- service1/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-func main() {
-	// implement GetData
-
-	// 30% possibility to return error
-}
---
+++ service1/server/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"log"
+	"net"
+	pb "service1/contract"
+)
+
+type server struct {
+	pb.UnimplementedGetDataFromService1Server
+}
+
+func (s *server) GetData(_ context.Context, request *pb.Service1Request) (*pb.Service1Response, error) {
+	data := map[int64]string{
+		1: "one",
+		2: "two",
+		3: "three",
+	}
+
+	resp, ok := data[request.GetId()]
+	if !ok {
+		return nil, status.Errorf(codes.NotFound, "not found")
+	}
+
+	return &pb.Service1Response{Message: resp}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterGetDataFromService1Server(s, &server{})
+	log.Printf("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
